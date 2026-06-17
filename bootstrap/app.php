76dc2ac8d4c75c79110172345f6d2fa11de0c01e
omino-781 <bacilomino781@gmail.com>
2026-06-17 17:33:33 +0300

    Trust proxies for CSRF fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
+        $middleware->trustProxies(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
